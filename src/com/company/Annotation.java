aed89ffd0aafda89f8d99c8708f8354df0a9f0a2
Eliseev Maksim <eliseevms01@mail.ru>
2021-10-20 17:07:57 +0300

    v1

---
+++ src/com/company/Annotation.java
@@ -0,0 +1,29 @@
+package com.company;
+import com.company.Figure;
+
+class Annotation {
+    private String label;
+    private Figure figure;
+
+    Annotation(Figure figure, String label) {
+        this.label = label;
+        this.figure = figure;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public static Annotation findByPoint(Annotation[] annotations, int x, int y) {
+        for (Annotation annotation : annotations) {
+            if (annotation.getFigure().containsPoint(new Point(x, y))) {
+                return annotation;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+import com.company.BoundingBox;
+import com.company.Circle;
+import com.company.Annotation;
+
+public class Main {
+
+    public static void main(String[] args) {
+        test1();
+        System.out.println();
+        test2();
+    }
+
+    private static void test1() {
+        BoundingBox box = new BoundingBox(new Point(0,10), new Point(100,0));
+        Circle circle = new Circle(new Point(100, 100), 50);
+        Annotation annotatedBox = new Annotation(box, "box");
+        Annotation annotatedCircle = new Annotation(circle, "circle");
+        Annotation[] annotations = new Annotation[]{annotatedBox, annotatedCircle};
+        Annotation a = Annotation.findByPoint(annotations,110,90); // circle
+        if (a != null) {
+            System.out.println(a.getLabel());
+            System.out.println(a.getFigure().getDescription());
+        } else {
+            System.out.println("Not found annotation by point");
+        }
+    }
+
+    private static void test2() {
+        BoundingBox box = new BoundingBox(new Point(0,10), new Point(100,0));
+        Circle circle = new Circle(new Point(100, 100), 50);
+        System.out.println(box.getDescription());
+        System.out.println("Move it");
+        box.move(5,10);
+        System.out.println(box.getDescription());
+
+        System.out.println();
+
+        System.out.println(circle.getDescription());
+        System.out.println("Move it");
+        circle.move(5,10);
+        System.out.println(circle.getDescription());
+    }
+}
